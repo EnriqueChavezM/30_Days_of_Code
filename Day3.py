7c1ad6031c91a9aed5a32a7a5084f10de88e91f1
Enrique_Ch <henri.ch.m.07@gmail.com>
2026-07-26 16:52:33 -0600

    feat(Day3): implement complete solution for day 3 example program
    Finalize logic and execution of the example program to validate system operation.

--- Day3.py
+++ Day3.py
@@ -30,3 +30,24 @@ import sys
 if __name__ == '__main__':
     N = int(raw_input().strip())
 """
+# troubleshooting
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input().strip())
+    if N >= 1 and N <= 100:
+        if N % 2 != 0:
+            print("Weird")
+        elif N >= 2 and N <= 5:
+            print("Not Weird")
+        elif N >= 6 and N <= 20:
+            print("Weird")
+        else:
+            print("Not Weird")
