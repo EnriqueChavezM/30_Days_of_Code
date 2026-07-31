2e12d13f1c9f0e08c6e5b38d86dbd27c07c1a94c
Enrique_Ch <henri.ch.m.07@gmail.com>
2026-07-31 11:31:55 -0600

    feat(Day10): implement complete solution for day 10 example program
    Finalize logic and execution of the example program to validate system operation.

--- Day10.py
+++ Day10.py
@@ -31,3 +31,45 @@ Initial Code
         n = int(input().strip())
 
 """
+#Troubleshooting
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def cambiobase (n):
+    x = n // 2
+    y = n % 2
+    if x == 0:
+        return str(y)
+    else:
+        return cambiobase(x) + str(y)  
+
+def conteo (*arr):
+    c = 0
+    c1 = []
+    for arr in arr:
+        if arr == "1":
+            c += 1
+
+        else:
+            c = 0
+        c1.insert(0, c)
+        c1.sort()
+        c1.reverse()    
+    return c1
+
+if __name__ == '__main__':
+    #n = int(input("Ingrese un número base 10: ").strip())
+    n = int(input().strip())
+    arr = list(cambiobase(n))
+    valor = int(conteo(*arr)[0])
+    """  
+    print("El número en base 2 es: " + ''.join(arr))
+    print("El número de unos en la representación binaria es: " + str(conteo(*arr)))
+    print("La cantidad máxima de unos consecutivos es: " + str(valor))
+    """
+    print(str(valor))
