70887f09e11dbb822c13bf18cd1b1dc4b7f91288
Enrique_Ch <henri.ch.m.07@gmail.com>
2026-07-31 11:18:20 -0600

    feat(day10): add example code for day 10
    Add new day file containing starter example code to serve as a base for upcoming implementation.

---
+++ Day10.py
@@ -0,0 +1,33 @@
+"""
+Objective
+    Today, we're working with binary numbers.
+
+Task
+    Given a base-10 integer, n, convert it to binary (base-2). Then find and print the base-10 integer denoting the maximum number of consecutive 1's in n's binary representation. When working with different bases, it is common to show the base as a subscript.
+
+Example
+    n = 125
+    The binary representation of 125 is 1111101. In base 2, there are 5 and 1 consecutive ones in two groups. Print the maximum, 5.
+
+Input Format
+    A single integer, n.
+
+Constraints
+    1 <= n <= 10^6
+
+Output Format
+    Print a single base-10 integer that denotes the maximum number of consecutive 1's in the binary representation of n.
+
+Initial Code
+    #!/bin/python3
+
+    import math
+    import os
+    import random
+    import re
+    import sys
+
+    if __name__ == '__main__':
+        n = int(input().strip())
+
+"""
